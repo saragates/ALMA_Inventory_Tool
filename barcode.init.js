e194385a1ad5ee1898a5c0c4c4977eb9c31b9de7
Sara Gates <75396544+saragates@users.noreply.github.com>
2021-03-16 16:55:18 -0500

    Update barcode.init.js
    Laura Rodriguez made this edit in Gitlab. I migrated it here.

--- barcode.init.js
+++ barcode.init.js
@@ -2,14 +2,15 @@ var API_SERVICE = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/";
 var API_REDIRECT = "php/barcodeReportRedirect.php";
 
 //Location validation Regular Expression
-var LOC_REGEX = /^main$/;
+var LOC_REGEX = /STACKS/;
 
 //Location format message
 var LOC_MSG = "Invalid Location";
 
 
 //Barcode validation Regular expression
-var BARCODE_REGEX = /^[0-9\-]{5,14}$/;
+var BARCODE_REGEX = /^[A-Z0-9\-]{5,14}$/;
 
 //Barcode Format message
-var BARCODE_MSG = "Enter a 5-14 digit barcode containing only numbers and dashes";
+var BARCODE_MSG = "Enter a 5-14 digit barcode containing only numbers, letters, and dashes";
+
